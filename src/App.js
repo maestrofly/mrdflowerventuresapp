2692d1cb5c64cb221ee26051c3f0b51f30085daa
DerrickDarku <derrickalberto@ymail.com>
2021-10-05 16:10:54 +0100

    background color change

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+       
+        <h1>
+          MRD FLOWER VENTURES
+        </h1>
+       
+        <p>
+        <a href="#"> SHOP NOW!</a>
+        </p>
+        
+    
+    </div>
+  );
+}
+
+export default App;
