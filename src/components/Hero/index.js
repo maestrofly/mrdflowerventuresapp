74b192a9d26dc373d8c680884a3dd6901d143e74
DerrickDarku <derrickalberto@ymail.com>
2021-11-07 12:49:25 +0000

    hero section completed

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from '../Navbar'
-import  { HeroContainer } from './HeroElements'
+import  { HeroContainer, HeroContent, HeroItems, HeroH1, 
+    HeroP, HeroBtn } from './HeroElements'
 
 const Hero = () => {
     return (
         <HeroContainer>
             <Navbar />
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Indoor and Outdoor plants for sale</HeroH1>
+                    <HeroP>Affordable plants at a go!</HeroP>
+                    <HeroBtn>Call us</HeroBtn>
+                </HeroItems>
+            </HeroContent>
         </HeroContainer>
     )
 }
