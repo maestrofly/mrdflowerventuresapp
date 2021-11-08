2762d916668a57f9768833d2b82bb2555afef13d
DerrickDarku <derrickalberto@ymail.com>
2021-11-08 13:38:17 +0000

    sidebar completed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { SidebarContainer, Icon, CloseIcon, SidebarMenu,
 SidebarLink, SidebarRoute, SideBtnWrap } from './SidebarElements'
 
-const Sidebar = () => {
+const Sidebar = ({isOpen, toggle}) => {
     return (
-        <SidebarContainer>
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
             <Icon>
                 <CloseIcon />
             </Icon>
            <SidebarMenu>
-               <SidebarLink to="/">Indoor</SidebarLink>
-               <SidebarLink to="/">Outdoor</SidebarLink>
-               <SidebarLink to="/">Medium</SidebarLink>
+               <SidebarLink to="/">Indoor plants</SidebarLink>
+               <SidebarLink to="/">Outdoor plants</SidebarLink>
+               <SidebarLink to="/">About Us</SidebarLink>
            </SidebarMenu>
            <SideBtnWrap>
                <SidebarRoute to="/">Make your order</SidebarRoute>
