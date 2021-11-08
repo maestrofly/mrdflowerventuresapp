b00fcfd33e3fb86cb2039372160e3036e1cdec77
DerrickDarku <derrickalberto@ymail.com>
2021-11-08 20:41:26 +0000

    products component completed

---
+++ src/components/Products/data.js
@@ -0,0 +1,31 @@
+import product1 from '../../images/Snakeplant.jpg';
+import product2 from '../../images/coleus.JPEG';
+import product3 from '../../images/dumbcane.jpg';
+
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Snake plant',
+        name: 'Snake plant',
+        desc: 'An indoor plant',
+        price: 'GHS40.00',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'coleus plant',
+        name: 'coleus plant',
+        desc: 'An outdoor plant',
+        price: 'GHS40.00',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'dumbcane plant',
+        name: 'Dumb cane plant',
+        desc: 'An indoor plant',
+        price: 'GHS40.00',
+        button: 'Add to Cart'
+    }
+]
